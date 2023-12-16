d01915f48aa9ab4eb67ec4d4aed5106e3e0e1424
Meet Shah <shahmeet622@gmail.com>
2023-12-16 18:05:06 +0530

    Fix: config change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Social-App",
+  // base: "/Social-App",
 
   plugins: [react()],
 });
